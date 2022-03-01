d1f05715f958d425345d7e6152ff956f6af60ff1
royal <royal_88@mail.ru>
2022-03-01 21:45:40 +0300

    my own initial commit

---
+++ Chapter04/build/CMakeFiles/list-swap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/list-swap.dir/list_swap.C.o"
+  "list-swap"
+  "list-swap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/list-swap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
